1bf0b4f8dd24dd6e42a3e61b1ced232ae1a8f64d
Fernanda Oliveira <foliveira@jazida.com>
2025-07-07 19:59:54 -0300

    first commit

---
+++ webintro/js/calculator/calculate.js
@@ -0,0 +1,6 @@
+const result = document.querySelector(".result");
+const buttonC = document.querySelector(".button-c");
+
+buttonC.addEventListener("click", function () {
+  result.innerText = 0;
+});
---
+++ webintro/js/exercise.js
@@ -0,0 +1,9 @@
+const loop = 10;
+const character = "☕";
+
+answer = "";
+for (let i = 0; i <loop; i++){
+    answer = answer + character;
+}
+
+console.log(answer);
